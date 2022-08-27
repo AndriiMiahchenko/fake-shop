a0a6434b32c335a86de80de6d14e4ecbb3604d3a
Andrii <miyagkiy12@gmail.com>
2022-08-27 18:47:47 +0300

    sss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const h1 = <h1>Hello world</h1>
+
+function App() {
+  return (
+    <div>
+      {h1}
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, odio et deleniti impedit quae eos tempora voluptatem, ipsam officia laborum dolor quo vel quasi consequuntur sint nisi placeat? Consectetur, obcaecati?</p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render("Hello world");
+root.render(<App/>);
